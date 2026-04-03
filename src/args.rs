37c3dde2232fba683035aa7a55fbbed4bbd6cd94
Mustafa ELrasheid <dev@mustafaelrasheid.com>
2026-04-03 15:53:33 +0300

    fixed stuff

---
+++ src/args.rs
@@ -0,0 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "rlogin")]
+#[command(version = "0.1.0")]
+#[command(author = "mustafaelrasheid")]
+#[command(
+    about = "login program that only uses yescript",
+    long_about = None
+)]
+pub struct Args {
+    pub path: Option<String>,
+}
